0001115d62c6263de290fc38bf381f537673218a
ndsanja <ndsanja@gmail.com>
2023-05-05 22:05:27 +0700

    create redirect base on cookies in app and auth

--- src/app/app/layout.tsx
+++ src/app/app/layout.tsx
@@ -1,8 +1,16 @@
+import { cookies } from 'next/dist/client/components/headers';
+import { redirect } from 'next/navigation';
+
 type Props = {
   children: React.ReactNode;
 };
 
 export default function AppLayout({ children }: Props) {
+  let authCookies = cookies().get('supabase-auth-token');
+  if (authCookies == undefined) {
+    redirect('/auth/login');
+  }
+
   return (
     <>
       <div>App Layout</div>
--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -1,6 +1,12 @@
+import { cookies } from 'next/dist/client/components/headers';
 import RegisterForm from './components/Form';
+import { redirect } from 'next/navigation';
 
 export default function RegisterPage() {
+  let authCookies = cookies().get('supabase-auth-token');
+  if (authCookies) {
+    redirect('/app');
+  }
   return (
     <div className="flex items-center justify-center w-full min-h-screen">
       <RegisterForm />
