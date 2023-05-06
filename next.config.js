ff5c0a16517b66838251e73002fda2874915f3e1
ndsanja <ndsanja@gmail.com>
2023-05-06 11:03:13 +0700

    test deploy and env backend

--- next.config.js
+++ next.config.js
@@ -1,27 +1,27 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   experimental: {
-//     appDir: true,
-//   },
-// };
-
-// module.exports = nextConfig;
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     appDir: true,
   },
-
-  transpilePackages: ['@mui/system', '@mui/material', '@mui/icons-material'],
-  modularizeImports: {
-    '@mui/material/?(((\\w*)?/?)*)': {
-      transform: '@mui/material/{{matches.[1]}}/{{member}}',
-    },
-    '@mui/icons-material/?(((\\w*)?/?)*)': {
-      transform: '@mui/icons-material/{{matches.[1]}}/{{member}}',
-    },
-  },
 };
 
 module.exports = nextConfig;
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   experimental: {
+//     appDir: true,
+//   },
+
+//   transpilePackages: ['@mui/system', '@mui/material', '@mui/icons-material'],
+//   modularizeImports: {
+//     '@mui/material/?(((\\w*)?/?)*)': {
+//       transform: '@mui/material/{{matches.[1]}}/{{member}}',
+//     },
+//     '@mui/icons-material/?(((\\w*)?/?)*)': {
+//       transform: '@mui/icons-material/{{matches.[1]}}/{{member}}',
+//     },
+//   },
+// };
+
+// module.exports = nextConfig;
